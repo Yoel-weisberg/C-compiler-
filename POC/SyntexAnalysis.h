b9a15b8bcafaea0b2eabe3decce67237c7cde1a1
Yoel weisberg <yoekweisberg5@gmail.com>
2024-08-22 19:32:11 +0300

    Created a SyntexError file

--- POC/SyntexAnalysis.h
+++ POC/SyntexAnalysis.h
@@ -1,7 +1,13 @@
 #pragma once
+#include "Constents.h"
+#include <vector>
+#include <iostream>
+#include <string>
+#include "SyntaxError.h"
 
 class SyntexAnalysis
 {
 public:
-	bool runSyntexAnalysis()
+	SyntexAnalysis(const std::vector<std::pair<std::string, Tokens>>& tokens);
+	
 };
