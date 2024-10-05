76d412b3c2ddae5cc4628930b82e86d19f3af340
Yoel weisberg <yoekweisberg5@gmail.com>
2024-10-05 20:49:54 +0300

    Installed llvm using cmake andf ninja

---
+++ LLVMPOC/LLVMPOC.cpp
@@ -0,0 +1,40 @@
+﻿#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Verifier.h"
+
+int main() {
+    llvm::LLVMContext Context;
+    llvm::Module* Module = new llvm::Module("MyModule", Context);
+    llvm::IRBuilder<> Builder(Context);
+
+    // Create a function signature: int add(int a, int b)
+    llvm::FunctionType* funcType = llvm::FunctionType::get(Builder.getInt32Ty(),
+        { Builder.getInt32Ty(), Builder.getInt32Ty() }, false);
+    llvm::Function* addFunc = llvm::Function::Create(funcType, llvm::Function::ExternalLinkage,
+        "add", Module);
+
+    // Create a basic block and set the insertion point
+    llvm::BasicBlock* BB = llvm::BasicBlock::Create(Context, "entry", addFunc);
+    Builder.SetInsertPoint(BB);
+
+    // Get function arguments
+    llvm::Function::arg_iterator args = addFunc->arg_begin();
+    llvm::Value* a = args++;
+    llvm::Value* b = args++;
+
+    // Add the two arguments
+    llvm::Value* sum = Builder.CreateAdd(a, b, "sum");
+
+    // Return the result
+    Builder.CreateRet(sum);
+
+    // Verify the function
+    llvm::verifyFunction(*addFunc);
+
+    // Output the LLVM IR to stdout
+    Module->print(llvm::outs(), nullptr);
+
+    delete Module;
+    return 0;
+}
--- POC/main.cpp
+++ POC/main.cpp
@@ -1,54 +1,96 @@
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <stdexcept>
-#include "Tokenizer.h"
-#include "SyntexAnalysis.h"
-#include "Optimiser.h"
-#include "ParseToAst.h"
-#include "Asm.h"
+//#include <iostream>
+//#include <fstream>
+//#include <sstream>
+//#include <stdexcept>
+//#include "Tokenizer.h"
+//#include "SyntexAnalysis.h"
+//#include "Optimiser.h"
+//#include "ParseToAst.h"
+//#include "Asm.h"
+//
+//
+//int main(int argc, char* argv[])
+//{
+//    try
+//    {
+//        if (argc != 2)  // argc should be 2, since argv[0] is the program name and argv[1] is the file path
+//        {
+//            throw std::invalid_argument("You need to specify the file you want to open");
+//        }
+//
+//        std::ifstream file(argv[1]);  // Open the file
+//        if (!file.is_open())
+//        {
+//            throw std::runtime_error("Could not open the specified file");
+//        }
+//
+//        std::stringstream buffer;
+//        buffer << file.rdbuf();  // Read file content into stringstream
+//        std::string file_content = buffer.str();  // Store content into a string
+//
+//        Tokeniser* tokeniser = new Tokeniser(file_content);
+//
+//        SyntexAnalysis* analysis = new SyntexAnalysis(tokeniser->getTokens());
+//
+//        Optimiser* optimiser = new Optimiser(tokeniser->getTokens());
+//
+//        Parser parser(optimiser->getOptimised());
+//
+//        ConvortASTtoASM* converter = new ConvortASTtoASM(parser.parseExpression());
+//
+//        file.close();  // Close the file
+//    }
+//    catch (const std::exception& e)
+//    {
+//        std::cerr << "Error: " << e.what() << std::endl;
+//        return 1;
+//    }
+//
+//    return 0;
+//}
+//
+//
+//
+//
+//
+//
 
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/Support/raw_ostream.h>
 
-int main(int argc, char* argv[])
-{
-    try
-    {
-        if (argc != 2)  // argc should be 2, since argv[0] is the program name and argv[1] is the file path
-        {
-            throw std::invalid_argument("You need to specify the file you want to open");
-        }
+int main() {
+    // Create a new LLVM context and a module to hold the function
+    llvm::LLVMContext context;
+    llvm::Module module("my_module", context);
 
-        std::ifstream file(argv[1]);  // Open the file
-        if (!file.is_open())
-        {
-            throw std::runtime_error("Could not open the specified file");
-        }
+    // Create an IR builder to help construct the LLVM IR
+    llvm::IRBuilder<> builder(context);
 
-        std::stringstream buffer;
-        buffer << file.rdbuf();  // Read file content into stringstream
-        std::string file_content = buffer.str();  // Store content into a string
+    // Define the function type (int32 (int32, int32)) - a function taking two int32 arguments and returning an int32
+    llvm::FunctionType* funcType = llvm::FunctionType::get(builder.getInt32Ty(), { builder.getInt32Ty(), builder.getInt32Ty() }, false);
 
-        Tokeniser* tokeniser = new Tokeniser(file_content);
+    // Create the function and insert it into the module
+    llvm::Function* addFunction = llvm::Function::Create(funcType, llvm::Function::ExternalLinkage, "add", &module);
 
-        SyntexAnalysis* analysis = new SyntexAnalysis(tokeniser->getTokens());
+    // Create a basic block and set the insertion point to this block
+    llvm::BasicBlock* entryBlock = llvm::BasicBlock::Create(context, "entry", addFunction);
+    builder.SetInsertPoint(entryBlock);
 
-        Optimiser* optimiser = new Optimiser(tokeniser->getTokens());
+    // Get function arguments
+    llvm::Function::arg_iterator args = addFunction->arg_begin();
+    llvm::Value* x = args++;
+    llvm::Value* y = args++;
 
-        Parser parser(optimiser->getOptimised());
+    // Generate code for x + y
+    llvm::Value* sum = builder.CreateAdd(x, y, "sum");
 
-        ConvortASTtoASM* converter = new ConvortASTtoASM(parser.parseExpression());
+    // Return the result
+    builder.CreateRet(sum);
 
-        file.close();  // Close the file
-    }
-    catch (const std::exception& e)
-    {
-        std::cerr << "Error: " << e.what() << std::endl;
-        return 1;
-    }
+    // Output the generated LLVM IR to the console
+    module.print(llvm::outs(), nullptr);
 
     return 0;
 }
-
-
-
-
