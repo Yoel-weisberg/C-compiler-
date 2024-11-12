2618c2d2c0d7d54d067986d8a4ec5814b8e3e087
Yoel-weisberg <120414345+Yoel-weisberg@users.noreply.github.com>
2024-11-12 16:55:01 +0200

    changed code to work with a code stream and not with a file

--- Preprocess.cpp
+++ Preprocess.cpp
@@ -1,204 +1,100 @@
 #include "Preprocess.h"
 #include <cctype>
 #include <algorithm>
+#include "SyntexError.h"
 
-void Preprocess::removeComments()
-{
-    
+void Preprocess::removeComments() {
+    // Implement comment removal logic using _fileRawContent.
 }
 
-void Preprocess::manageIncludes()
-{
-    // INCLUDE_IDENTIFIER 
-
+void Preprocess::manageIncludes() {
+    // Implement include management logic.
 }
 
-void Preprocess::handleMacroVariables()
-{
-    // std::fstream file (_fileName, std::ios::in | std::ios::out);
-    // if (!file.is_open()){
-    //     std::cerr << "[PREPROCESSOR ERROR]  Unable to open file" << std::endl;
-    //     return;
-    // }
-    // char ch;
-    // while (file.get(ch)) {
-    //     int currentLine = 0;
-    //     int currentChar = 0;
-    //     if (ch == '#') {
-    //         std::string macroName;
-    //         while (file.get(ch) && ch!= ' ') {
-    //             macroName += ch;
-    //         }
-    //         if (macroName == "define") {
-    //             std::string macroValue;
-    //             std::string macroKey;
-    //             bool multyLine = false;
-    //             bool gotEnd = false;
-    //             // moving pointer to start of macro key 
-    //             while (file.get(ch) && ch == ' ') {}
-                
-    //             // getting macro key
-    //             while (file.get(ch) && ch!= ' ') {
-    //                 macroKey += ch; 
-    //             }
+void Preprocess::handleMacroVariables() {
+    size_t pos = 0;
+    int currentLine = 0;
+    while (pos < _fileRawContent.size()) {
+        char ch = _fileRawContent[pos++];
+        if (ch == '#') {
+            std::string macroName;
+            while (pos < _fileRawContent.size() && (ch = _fileRawContent[pos++]) != ' ') {
+                macroName += ch;
+            }
+            if (macroName == "define") {
+                std::string macroKey, macroValue;
+                bool multyLine = false, gotEnd = false;
 
-    //             // moving pointer to start of macro value
-    //             while (file.get(ch) && ch==' '){}
-                
-    //             // getting macro value
-    //             while (file.get(ch) && !gotEnd) {
-    //                 macroValue += ch; 
-    //                 // checking if file needs to continue
-    //                 if (ch == '\\')
-    //                 {
-    //                     multyLine = true; 
-    //                 }
-    //                 if (ch == '\n')
-    //                 {
-    //                     multyLine ? gotEnd = false : gotEnd = true;
-    //                     multyLine = false;
-    //                     currentLine++;
-    //                 }
-    //             }
+                // Skip spaces before macro key
+                while (pos < _fileRawContent.size() && (ch = _fileRawContent[pos]) == ' ') pos++;
 
-    //             // checking that the key is valid
-    //             checkMacroKeyValidity(macroKey) ? throw SyntaxError("Macro key not valid", currentLine) : true;
+                // Get macro key
+                while (pos < _fileRawContent.size() && (ch = _fileRawContent[pos]) != ' ') {
+                    macroKey += ch;
+                    pos++;
+                }
 
-    //             // checking that the value is valid
-    //             checkMacroValueValidity(macroValue)? throw SyntaxError("Macro value not valid", currentLine) : true;
+                // Skip spaces before macro value
+                while (pos < _fileRawContent.size() && (ch = _fileRawContent[pos]) == ' ') pos++;
 
-    //             // storing macro in the table
-    //             _macroTable[macroKey] = macroValue;
+                // Get macro value
+                while (pos < _fileRawContent.size() && !gotEnd) {
+                    ch = _fileRawContent[pos++];
+                    macroValue += ch;
+                    if (ch == '\\') {
+                        multyLine = true;
+                    }
+                    if (ch == '\n') {
+                        multyLine ? gotEnd = false : gotEnd = true;
+                        multyLine = false;
+                        currentLine++;
+                    }
+                }
 
-    //         } else if (macroName == "undef") {
-    //             // TODO  - remove define from macro table 
-    //         } else if (macroName == "include") {
-    //             // TODO - manage include 
-    //         }
-    //     } else {
-    //         file.putback(ch);
-    //     }
-    // }
-    // // replace the macros in the macro table 
-    // replaceMacro();
-}
+                // Check key and value validity
+                if (!checkMacroKeyValidity(macroKey)) throw SyntaxError("Macro key not valid", currentLine);
+                if (!checkMacroValueValidity(macroValue)) throw SyntaxError("Macro value not valid", currentLine);
 
-std::string Preprocess::replaceMacro()
-{
-    // std::fstream file (_fileName, std::ios::in | std::ios::out);
-    // if (!file.is_open()){
-    //     throw SyntaxError("Invalid file name", 0);
-    // }
-    // char ch;
-    // std::string codeStream, currentBlock;
-    // bool isThereQuetes = false, singleQuete = false;
-    // while (file.get(ch))
-    // {
-    //     // were not appling macros inside strings 
-    //     (ch == '"' && !singleQuete)  ? isThereQuetes = !isThereQuetes : true;
-    //     (ch == '\'' && !isThereQuetes) ? singleQuete = !singleQuete : true;
+                _macroTable[macroKey] = macroValue;
 
-    //     if ((ch == ' ' || ch == '\n') && !isThereQuetes)
-    //     {
-    //         if (currentBlock.empty()) {
-    //             continue;
-    //         }
-    //         auto it = _macroTable.find(currentBlock);
-    //         if (it!= _macroTable.end()) {
-    //             codeStream += getFinalValue(it->first);
-    //         } else {
-    //             codeStream += currentBlock;
-    //         }
-    //         currentBlock.clear();
-    //     }
-
-    // }
-    
-}
-
-bool Preprocess::checkMacroKeyValidity(const std::string &macroKey)
-{
-    if (macroKey.empty()) {
-        return false; // Empty strings are not valid identifiers
-    }
-
-    // Check the first character: it must be a letter or underscore
-    if (!(std::isalpha(macroKey[0]) || macroKey[0] == '_')) {
-        return false;
-    }
-
-    // Check the rest of the characters: they must be letters, digits, or underscores
-    for (size_t i = 1; i < macroKey.length(); ++i) {
-        if (!(std::isalnum(macroKey[i]) || macroKey[i] == '_')) {
-            return false;
+            } else if (macroName == "undef") {
+                // TODO: Remove define from macro table
+            } else if (macroName == "include") {
+                // TODO: Manage include
+            }
         }
     }
 
-    return true;  
+    // Replace macros in the macro table
+    replaceMacro();
 }
 
-bool Preprocess::checkMacroValueValidity(const std::string &macroValue)
-{
-    // if (macroValue.empty()) { return false; }
+std::string Preprocess::replaceMacro() {
+    std::string codeStream;
+    std::string currentBlock;
+    bool isThereQuotes = false, singleQuote = false;
 
-    // // if the macro is a string
-    // if (macroValue[0] == '"')
-    // {
-    //     bool escapeChar = false;
-    //     // we only need to check that it ends with a quote
-    //     if (macroValue[macroValue.length() - 1] != '"') {return false;}
-    //     // checking that it is a vlid string - it dosent contain quotes
-    //     for (int i = 1; i < macroValue.length() - 1; i++)
-    //     {
-    //         if (macroValue[i] == '\\')
-    //         {
-    //             escapeChar = true;
-    //         }
-    //         else if (macroValue[i] == '"')
-    //         {
-    //             if (!escapeChar)
-    //             {
-    //                 return false;
-    //             }
-    //             escapeChar = false;
-    //         }
-    //         else
-    //         {
-    //             escapeChar = false;
-    //         }
-    //     }
-    // }
-    // if (isNumber(macroValue))
-    // {
-    //     return true;
-    // }
-    // if (macroValue[0] == '\'')
-    // {
-    //     if (macroValue[macroValue.length() - 1] != '\'') {return false;}
-    //     if (macroValue.length() == 1 || (macroValue[1] == '/' && macroValue.length() == 2))
-    //     {
-    //         return true;
-    //     }
-    //     return false;
-    // }
-    // // a macro can hold a different macro
-    // if (checkMacroKeyValidity(macroValue)) { return true;}
-    // return false;
-}
+    for (size_t pos = 0; pos < _fileRawContent.size(); ++pos) {
+        char ch = _fileRawContent[pos];
 
-bool Preprocess::isNumber(const std::string &number)
-{
-    return !number.empty() && std::all_of(number.begin(), number.end(), ::isdigit);
-}
+        (ch == '"' && !singleQuote) ? isThereQuotes = !isThereQuotes : true;
+        (ch == '\'' && !isThereQuotes) ? singleQuote = !singleQuote : true;
 
-std::string Preprocess::getFinalValue(std::string key)
-{
-    if (_macroTable.find(_macroTable.find(key)->second) == _macroTable.end())
-    {
-        return _macroTable.find(key)->second;
+        if ((ch == ' ' || ch == '\n') && !isThereQuotes) {
+            if (currentBlock.empty()) continue;
+
+            auto it = _macroTable.find(currentBlock);
+            if (it != _macroTable.end()) {
+                codeStream += getFinalValue(it->first);
+            } else {
+                codeStream += currentBlock;
+            }
+            currentBlock.clear();
+        } else {
+            currentBlock += ch;
+        }
     }
-    else
-    {
-        return getFinalValue(_macroTable.find(key)->second);
-    }
+    return codeStream;
 }
+
+// The rest of your functions remain the same
