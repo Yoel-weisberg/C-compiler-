99ad7bdc651f63601e13a04549095531983ef2be
Neta1910 <netaorevi1910@gmail.com>
2024-12-10 09:30:10 +0200

    Added codegen for pointers to the stack, Hopfully works!

--- FinalProjectYoelAndNeta/cpp/ExprAST.cpp
+++ FinalProjectYoelAndNeta/cpp/ExprAST.cpp
@@ -17,26 +17,8 @@ Value* CharExprAST::codegen()
 
 Value* ptrExprAST::codegen()
 {
-    ////return ConstantInt::get(Helper::getContext(), )
-    //const int addr = std::stoi(_valAsStr);
-    //llvm::Type* = 
-    ////return ConstantExpr::getIntToPtr(, llvm::PointerType::getUnqual(Type::getInt32Ty));
-    //llvm::ConstantInt * beg = ConstantInt::get(llvm::Type::getInt32Ty, (uint64_t)&addr);
-    //ConstantInt::get()
-    //return ConstantInt::get(Helper::getContext(), APInt(_size, std::stoi(_valAsStr)));
-    ////return ConstantPointerNull::get(llvm::PointerType::getInt32Ty);
-    //uint64_t value;
-    //_valAsStr >> value;
-    //iss >> value;
-    //std::hex(_valAsStr);
-    //auto s = reinterpret_cast<uintptr_t>(_valAsStr);
-    uint64_t res = Helper::hexToDec(_valAsStr);
-    std::cout << " " << _valAsStr << " String to int --> " << std::stoi(_valAsStr) << std::endl;
-    Constant* con = ConstantInt::get(Type::getInt64Ty(Helper::getContext()), (uint64_t)std::stoi(_valAsStr)); 
-    //Constant* Sb = ConstantInt::get(Type::getInt64Ty(Helper::getContext()),value);
-
-    return ConstantExpr::getIntToPtr(con, PointerType::getUnqual(Type::getInt64Ty(Helper::getContext())));
-    //return PointerType::get(Helper::getContext(), Type::getPointerAddressSpace());
+    Constant* con = ConstantInt::get(Type::getInt64Ty(Helper::getContext()), Helper::hexToDec(_valAsStr));
+    return ConstantExpr::getIntToPtr(con, PointerType::getUnqual(Helper::getBuilder().getInt32Ty()));
 }
 
 
@@ -55,19 +37,6 @@ Value* VariableExprAST::codegen()
 Value* AssignExprAST::codegen() {
     // Check if the variable already exists in the symbol table
     auto symbolOpt = Helper::symbolTable.findSymbol(_varName);
-    //if (!symbolOpt) {
-    //    //// Variable is not declared, allocate and add it to the symbol table
-    //    //Value* varAddress = Helper::allocForNewSymbol(_varName, _varType);
-
-    //    //// Add the variable to the symbol table
-    //    //Helper::symbolTable.add(_varName, _varType, varAddress);
-    //    //AssignExprAST* x = std::make_unique<AssignExprAST>(_value)->getValue();
-    //    //Helper::addSymbol(_varName, _varType);
-
-    //    // Re-fetch the symbol from the symbol table
-    //    symbolOpt = Helper::symbolTable.findSymbol(_varName);
-    //}
-
     if (!symbolOpt) {
         std::cerr << "Error: Failed to retrieve the variable '" << _varName << "' after adding it.\n";
         return nullptr;
