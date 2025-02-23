ab559d58f09a2f1f137cd7d48c04fc9b5f608c54
Yoel-weisberg <120414345+Yoel-weisberg@users.noreply.github.com>
2025-02-23 17:32:57 +0200

    made it work

--- LiveShare/Client/Client.js
+++ LiveShare/Client/Client.js
@@ -1,32 +1,54 @@
-import {Peer} from "https://esm.sh/peerjs@1.5.4?bundle-deps"
-import { ResponseCodes } from "../PacketCodes";
+
+const RequestCodes = {
+  INITIAL_FETCH: 100,
+  FETCH_FILE: 101,
+  UPDATE: 102
+};
+
+const ResponseCodes = {
+  INITIAL_FETCH: 200,
+  FETCH_FILE: 201,
+  CHANGE_DETECTED: 202
+};
 
 export default class Client {
-    constructor (peerId){
-        this.peerId = peerId;
-        this.peer = new Peer()
-        this.conn = this.peer.connect(this.peerId);
+  constructor(peerId) {
+    this.peerId = peerId;
+    this.peer = new Peer();
+    this.conn = this.peer.connect(this.peerId);
+    this.counter = 0;
 
-        conn.on('open', function() {
-            // Receive messages
-            conn.on('data', function(data) {
-              const packetCode = data.PacketCode;
-              switch(packetCode) {
-                case ResponseCodes.INITIAL_FETCH:
-                    console.log('Received INITIAL_FETCH');
-                    break;
-                case ResponseCodes.FETCH_FILE:
-                    console.log('Received FETCH_FILE');
-                    break;
-                case ResponseCodes.CHANGE_DETECTED:
-                    console.log('Received CHANGE_DETECTED, change: ', data.data);
-                    break;
-                case ResponseCodes.FILE_UPDATED:
-                default:
-              }
-            });
-        
-          });
+    // Create a reference to the display element
+    this.counterElement = document.getElementById("counter-display");
+
+    this.conn.on("open", () => {
+      console.log("Connected to peer:", this.peerId);
+      // Receive messages
+      this.conn.on("data", (data) => {
+        const packetCode = data.PacketCode;
+        switch (packetCode) {
+          case ResponseCodes.INITIAL_FETCH:
+            console.log("Received INITIAL_FETCH");
+            this.updateCounter(data.data);
+            break;
+          case ResponseCodes.FETCH_FILE:
+            console.log("Received FETCH_FILE");
+            break;
+          case ResponseCodes.CHANGE_DETECTED:
+            console.log("Received CHANGE_DETECTED, change: ", data.data);
+            this.updateCounter(data.data);
+            break;
+          case ResponseCodes.FILE_UPDATED:
+          default:
+        }
+      });
+    });
+  }
+
+  updateCounter(newValue) {
+    this.counter = newValue;
+    if (this.counterElement) {
+      this.counterElement.textContent = `Counter: ${this.counter}`;
     }
-
+  }
 }
